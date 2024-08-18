2ed9a101196f4fc6d75781f8a65cc314663c816c
aditya <adityapandeyadp@gmail.com>
2024-08-18 18:53:05 +0530

    lil on home

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
